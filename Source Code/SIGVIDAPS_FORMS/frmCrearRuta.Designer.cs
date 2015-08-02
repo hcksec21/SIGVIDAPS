bd658e2aafd65f9b4ef4b5fab17279f9478c87b7
JoseMiguel <hcksec21@gmail.com>
2015-08-02 06:20:47 -0500

    cambios rutas

--- Source Code/SIGVIDAPS_FORMS/frmCrearRuta.Designer.cs
+++ Source Code/SIGVIDAPS_FORMS/frmCrearRuta.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtApellidos = new System.Windows.Forms.MaskedTextBox();
+            this.txtLugarLlegada = new System.Windows.Forms.MaskedTextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtNombres = new System.Windows.Forms.MaskedTextBox();
+            this.txtLugarSalida = new System.Windows.Forms.MaskedTextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvRutas = new System.Windows.Forms.DataGridView();
             this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LugarSalida = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -42,7 +42,7 @@
             this.btnCancelar = new System.Windows.Forms.Button();
             this.btnGuardar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRutas)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -65,56 +65,56 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Registro de nueva Ruta";
             // 
-            // txtApellidos
+            // txtLugarLlegada
             // 
-            this.txtApellidos.Location = new System.Drawing.Point(324, 165);
-            this.txtApellidos.Name = "txtApellidos";
-            this.txtApellidos.PromptChar = ' ';
-            this.txtApellidos.Size = new System.Drawing.Size(290, 20);
-            this.txtApellidos.TabIndex = 12;
+            this.txtLugarLlegada.Location = new System.Drawing.Point(329, 162);
+            this.txtLugarLlegada.Name = "txtLugarLlegada";
+            this.txtLugarLlegada.PromptChar = ' ';
+            this.txtLugarLlegada.Size = new System.Drawing.Size(290, 20);
+            this.txtLugarLlegada.TabIndex = 12;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.SlateGray;
-            this.label3.Location = new System.Drawing.Point(113, 160);
+            this.label3.Location = new System.Drawing.Point(118, 157);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(196, 25);
             this.label3.TabIndex = 11;
             this.label3.Text = "Lugar de Llegada";
             // 
-            // txtNombres
+            // txtLugarSalida
             // 
-            this.txtNombres.Location = new System.Drawing.Point(324, 135);
-            this.txtNombres.Name = "txtNombres";
-            this.txtNombres.PromptChar = ' ';
-            this.txtNombres.Size = new System.Drawing.Size(290, 20);
-            this.txtNombres.TabIndex = 10;
+            this.txtLugarSalida.Location = new System.Drawing.Point(329, 132);
+            this.txtLugarSalida.Name = "txtLugarSalida";
+            this.txtLugarSalida.PromptChar = ' ';
+            this.txtLugarSalida.Size = new System.Drawing.Size(290, 20);
+            this.txtLugarSalida.TabIndex = 10;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.SlateGray;
-            this.label2.Location = new System.Drawing.Point(113, 130);
+            this.label2.Location = new System.Drawing.Point(118, 127);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(178, 25);
             this.label2.TabIndex = 9;
             this.label2.Text = "Lugar de Salida";
             // 
-            // dataGridView1
+            // dgvRutas
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvRutas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRutas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ID,
             this.Codigo,
             this.LugarSalida,
             this.LugarLlegada});
-            this.dataGridView1.Location = new System.Drawing.Point(20, 207);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(699, 180);
-            this.dataGridView1.TabIndex = 13;
+            this.dgvRutas.Location = new System.Drawing.Point(20, 207);
+            this.dgvRutas.Name = "dgvRutas";
+            this.dgvRutas.Size = new System.Drawing.Size(699, 180);
+            this.dgvRutas.TabIndex = 13;
             // 
             // ID
             // 
@@ -168,17 +168,17 @@
             this.ClientSize = new System.Drawing.Size(738, 466);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnGuardar);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.dgvRutas);
+            this.Controls.Add(this.txtLugarLlegada);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtLugarSalida);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "frmCrearRuta";
             this.Text = "frmCrearRuta";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRutas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,11 +188,11 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.MaskedTextBox txtApellidos;
+        private System.Windows.Forms.MaskedTextBox txtLugarLlegada;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.MaskedTextBox txtNombres;
+        private System.Windows.Forms.MaskedTextBox txtLugarSalida;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvRutas;
         private System.Windows.Forms.DataGridViewTextBoxColumn ID;
         private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
         private System.Windows.Forms.DataGridViewTextBoxColumn LugarSalida;
